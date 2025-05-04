df34a2231d3e40cb91309712f1d8cc2886e53e08
Chaitanya Patil <157099340+Sai-Pat@users.noreply.github.com>
2025-05-04 18:06:19 +0530

    2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,67 +1,58 @@
 #include <iostream>
-#include <stack>
 using namespace std;
 
-class Graph {
+class G {
 public:
+    int adjm[10][10];
     int V;
-    int** adjMatrix;
 
-    Graph(int vertices) {
-        V = vertices;
-        adjMatrix = new int*[V];
-        for (int i = 0; i < V; i++) {
-            adjMatrix[i] = new int[V];
-            for (int j = 0; j < V; j++) {
-                adjMatrix[i][j] = 0;
-            }
-        }
+    G(int vor) {
+        V = vor;
+        for (int i = 0; i < V; i++)
+            for (int j = 0; j < V; j++)
+                adjm[i][j] = 0;
     }
 
-    void addEdge(int u, int v) {
-        adjMatrix[u][v] = 1;
-        adjMatrix[v][u] = 1;
+    void edge(int u, int v) {
+        adjm[u][v] = 1;
+        adjm[v][u] = 1;
     }
 
-    void DFS(int startVertex) {
-        bool* visited = new bool[V];
-        for (int i = 0; i < V; i++) {
-            visited[i] = false;
-        }
+    void performDFS(int start) {
+        bool visited[10] = {false};
+        int stack[10]; // simple array stack
+        int top = -1;
 
-        stack<int> s;
-        s.push(startVertex);
+        stack[++top] = start;
 
-        while (!s.empty()) {
-            int vertex = s.top();
-            s.pop();
+        cout << "DFS Starting...\n";
 
-            if (!visited[vertex]) {
-                visited[vertex] = true;
-                cout << vertex << " ";
-            }
+        while (top != -1) {
+            int curr = stack[top--];
 
-            for (int i = V - 1; i >= 0; i--) {
-                if (adjMatrix[vertex][i] == 1 && !visited[i]) {
-                    s.push(i);
+            if (!visited[curr]) {
+                visited[curr] = true;
+                cout << curr << " ";
+
+                // Push all unvisited neighbors to stack (in reverse order for correct DFS)
+                for (int i = V - 1; i >= 0; i--) {
+                    if (adjm[curr][i] == 1 && !visited[i]) {
+                        stack[++top] = i;
+                    }
                 }
             }
         }
-        cout << endl;
     }
 };
 
 int main() {
-    Graph g(5);
+    G g(5);
+    g.edge(0, 1);
+    g.edge(0, 2);
+    g.edge(1, 3);
+    g.edge(1, 4);
+    g.edge(2, 3);
 
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 3);
-    g.addEdge(1, 4);
-    g.addEdge(2, 3);
-
-    cout << "DFS starting from vertex 0: ";
-    g.DFS(0);
-
+    g.performDFS(0);
     return 0;
 }
